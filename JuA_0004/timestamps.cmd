1a774d78b378c883e1add8d4c790604be4aede6e
LZP-2020-1-0200 <76963783+LZP-2020-1-0200@users.noreply.github.com>
2021-10-07 12:18:03 +0300

    Update timestamps.cmd

--- JuA_0004/timestamps.cmd
+++ JuA_0004/timestamps.cmd
@@ -1,8 +1,5 @@
 @echo off
 REM finds windows timestamps of files in current directory and subdirectories
-REM inspired by
-REM https://superuser.com/questions/937380/get-creation-time-of-file-in-milliseconds
-REM juris.prikulis@lu.lv 11/15/2018
 REM use at your own risk
 REM works on XP and 10 and probably some other versions
 setlocal enabledelayedexpansion
